8837113ef37d77994840aab42aa185568f3107a2
Sina <Sinamatinrad80@Gmail.com>
2024-05-10 15:35:57 +0330

    Encapsulation with access

--- School.cs
+++ School.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace SchoolLibrary
 {
-    internal class School
+    public class School
     {
-        string Name { get; set; }
-        string Address { get; set; }
-        string  City { get; set; }
-        string State { get; set; }
-        string Zip {  get; set; }
-         string PhoneNumber { get; set; }
-        string _twitterAddress;
-        string TwitterAddress
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        private string _twitterAddress;
+        public string TwitterAddress
         {
             //make sure the twitter address starts with @
             get { return _twitterAddress; }
