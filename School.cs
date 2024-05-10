b4b3bd2e39e79adf7cdc9c84327ca87171206f45
Sina <Sinamatinrad80@Gmail.com>
2024-05-10 15:00:14 +0330

    Add project files.

---
+++ School.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibrary
+{
+    internal class School
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+        string  City { get; set; }
+        string State { get; set; }
+        string Zip {  get; set; }
+         string PhoneNumber { get; set; }
+        string _twitterAddress;
+        string TwitterAddress
+        {
+            //make sure the twitter address starts with @
+            get { return _twitterAddress; }
+            set
+            {
+                if (value.StartsWith("@")
+                {
+                    _twitterAddress = value;
+                }
+                else
+                {
+                    throw new Exception("The twitter address must begin with @");
+                }
+            }
+        }
+
+    }
+}
